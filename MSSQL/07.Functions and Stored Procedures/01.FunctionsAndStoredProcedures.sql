ea94c5e2695964363e7731a0313578f739a69fd2
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2023-10-04 14:37:27 +0300

    Add more queries

--- MSSQL/07.Functions and Stored Procedures/01.FunctionsAndStoredProcedures.sql
+++ MSSQL/07.Functions and Stored Procedures/01.FunctionsAndStoredProcedures.sql
@@ -282,3 +282,27 @@ CREATE OR ALTER PROCEDURE [usp_CalculateFutureValueForAccount] (@accountId  INT,
 --13.	*Scalar Function: Cash in User Games Odd Rows
 --Create a function ufn_CashInUsersGames that sums the cash of the odd rows. Rows must be ordered by cash in descending order. The function should take a game name as a parameter and return the result as a table. Submit only your function in.
 --Execute the function over the following game names, ordered exactly like: "Love in a mist".
+  
+  
+
+  CREATE OR ALTER FUNCTION [ufn_CashInUsersGames](@gameName NVARCHAR(50))
+		     RETURNS TABLE
+                        AS
+					RETURN
+						    SELECT SUM([Cash]) AS [SumCash]
+							  FROM (
+									  SELECT [Cash] 
+											 ,ROW_NUMBER() OVER( ORDER BY [ug].[Cash] DESC) AS [Row]
+										FROM [Games] AS [g]
+								  INNER JOIN [UsersGames] AS [ug] ON [g].[Id] = [ug].[GameId]
+								       WHERE [Name] = @gameName
+								   ) 
+								AS [SubQuery]
+  
+							 WHERE [Row] % 2 = 1 
+
+					    GO
+
+SELECT * FROM [dbo].[ufn_CashInUsersGames]('Love in a mist')
+
+	GO
