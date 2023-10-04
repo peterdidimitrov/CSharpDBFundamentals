b3c8e5ca0d9db949cfdbae3d9f31c42c549034fd
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2023-10-04 16:22:03 +0300

    Add more queries

---
+++ MSSQL/08.Triggers and Transactions/01.TriggersAndTransactions.sql
@@ -0,0 +1,4 @@
+--1.	Create Table Logs
+--Create a table – Logs (LogId, AccountId, OldSum, NewSum). Add a trigger to the Accounts table that enters a new entry into the Logs table every time the sum on an account change. Submit only the query that creates the trigger.
+
+
