5444c873712ab613f26a38b2982d8aa84c03b22e
peterdidimitrov <petr_dimitrov@abv.bg>
2023-12-06 00:49:48 +0200

    Add the last two methods in Theatre project

--- EntityFrameworkCore/ExamPreparation/06.CSharpDBAdvancedExam-04Dec2021/01. Model Defition_Skeleton/Theatre/DataProcessor/Serializer.cs
+++ EntityFrameworkCore/ExamPreparation/06.CSharpDBAdvancedExam-04Dec2021/01. Model Defition_Skeleton/Theatre/DataProcessor/Serializer.cs
@@ -1,19 +1,69 @@
 ﻿namespace Theatre.DataProcessor
 {
-
+    using Newtonsoft.Json;
     using System;
     using Theatre.Data;
+    using Theatre.DataProcessor.ExportDto;
+    using Trucks.Utilities;
 
     public class Serializer
     {
         public static string ExportTheatres(TheatreContext context, int numbersOfHalls)
         {
-            throw new NotImplementedException();
+            var theatres = context.Theatres
+                .Where(t => t.NumberOfHalls >= numbersOfHalls && t.Tickets.Count >= 20)
+                .ToArray()
+                .Select(t => new
+                {
+                    Name = t.Name,
+                    Halls = t.NumberOfHalls,
+                    TotalIncome = t.Tickets
+                    .Where(t => t.RowNumber >= 1 && t.RowNumber <= 5)
+                    .Sum(t => t.Price),
+                    Tickets = t.Tickets
+                    .Where(t => t.RowNumber >= 1 && t.RowNumber <= 5)
+                    .Select(tc => new
+                    {
+                        tc.Price,
+                        tc.RowNumber
+                    })
+                    .OrderByDescending(tc => tc.Price)
+                })
+                .OrderByDescending(t => t.Halls)
+                .ThenBy(t => t.Name)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(theatres, Formatting.Indented);
         }
 
         public static string ExportPlays(TheatreContext context, double raiting)
         {
-            throw new NotImplementedException();
+            XmlHelper xmlHelper = new XmlHelper();
+
+            ExportPlayDto[] plays = context.Plays
+                .Where(p => p.Rating <= raiting)
+                .ToArray()
+                .Select(p => new ExportPlayDto
+                {
+                    Title = p.Title,
+                    Duration = p.Duration.ToString("c"),
+                    Rating = p.Rating == 0? "Premier": p.Rating.ToString(),
+                    Genre = p.Genre.ToString(),
+                    Actors = p.Casts
+                    .Where(a => a.IsMainCharacter == true)
+                    .Select(a => new ExportActorDto
+                    {
+                        FullName = a.FullName,
+                        MainCharacter = $"Plays main character in '{p.Title}'."
+                    })
+                    .OrderByDescending(a => a.FullName)
+                    .ToArray()
+                })
+                .OrderBy(p => p.Title)
+                .ThenBy(p => p.Genre)
+                .ToArray();
+
+            return xmlHelper.Serialize<ExportPlayDto[]>(plays, "Plays");
         }
     }
 }
